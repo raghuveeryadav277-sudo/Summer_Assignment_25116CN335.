cce662140564ee71568bdba6113508937dd12cdf
Raghubir Kumar <raghuveeryadav277@gmail.com>
2026-06-10 17:27:55 +0530

    Added day - 10 Assignment

---
+++ day - 10/ABCPYRAMID.java
@@ -0,0 +1,18 @@
+public class ABCPYRAMID {
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=1 ; i<=n ; i++){
+            for(int j=1 ; j<=n-i ; j++){
+                System.out.print("  ");
+
+            }
+            for(int j=1 ; j<=i ; j++){
+                System.out.print(" " + (char)(64+j));
+            }
+            for(int j=i-1 ; j>=1 ; j--){
+                System.out.print( " "+(char)( 'A'+j-1));
+            }
+            System.out.println();
+        }
+    }
+}
